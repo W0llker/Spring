f2707087bf37f23a6333c097a73c76662f35797a
W0llker <118970644+W0llker@users.noreply.github.com>
2023-10-12 10:51:25 +0300

    Spring

---
+++ HelloSpring/src/main/java/annotation/FirstApp.java
@@ -0,0 +1,10 @@
+package annotation;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FirstApp {
+    public void hello() {
+        System.out.println("Hello Spring");
+    }
+}
